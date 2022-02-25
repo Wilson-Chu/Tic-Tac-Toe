792ded0190913456224ee0478f63cabdba0833cc
Wilson Chu <wilson1.chu@gmail.com>
2022-02-24 23:14:16 -0500

    setup initial grid array and object constructor

--- tictactoe.js
+++ tictactoe.js
@@ -1,1 +1,18 @@
 let gridArray = [];
+
+function gameBoard(player, state) {
+    this.player = player;
+    this.state = state;
+}
+
+const player1 = new gameBoard('Player 1', 'X');
+
+/* *** Factory function of gameBoard object ***
+const gameBoard = function (name, state) {
+    return { name, state };
+};
+
+const player1 = gameBoard('Player 1', 'X');
+console.log(player1.name); // 'Player 1'
+
+*/
