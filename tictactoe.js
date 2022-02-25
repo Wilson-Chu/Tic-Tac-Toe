6df55f351475d4daa233e0c02913a518a262dcc5
Wilson Chu <wilson1.chu@gmail.com>
2022-02-24 23:49:39 -0500

    setup boilerplate module and factory function for players

--- tictactoe.js
+++ tictactoe.js
@@ -1,18 +1,18 @@
 let gridArray = [];
 
-function gameBoard(player, state) {
-    this.player = player;
-    this.state = state;
-}
+// Creating gameBoard module to setup the grid
+const gameBoard = (function () {
+    //enter stuff here...
 
-const player1 = new gameBoard('Player 1', 'X');
 
-/* *** Factory function of gameBoard object ***
-const gameBoard = function (name, state) {
+})();
+
+// Factory function for player objects
+const playerFactory = function (name, state) {
     return { name, state };
 };
 
-const player1 = gameBoard('Player 1', 'X');
+const player1 = playerFactory('Player 1', 'X');
+const player2 = playerFactory('Computer', 'O');
 console.log(player1.name); // 'Player 1'
-
-*/
+console.log(player2.state); // 'O'
